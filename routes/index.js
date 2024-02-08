e186f6b7fb976316ffaa62355088e4d95e879428
GabyAM <gdgabyam@gmail.com>
2024-02-08 12:51:59 -0300

    adds functionality to the form
    now the form adds a message to the messages array when submitted

--- routes/index.js
+++ routes/index.js
@@ -19,6 +19,17 @@ router
     .get(function (req, res, next) {
         res.render('form');
     })
+    .post(function (req, res, next) {
+        const messageUser = req.body.messageUser;
+        const messageText = req.body.messageText;
+        messages.push({
+            text: messageText,
+            user: messageUser,
+            added: new Date()
+        });
+        res.redirect('/');
+    });
+
 router.get('/', function (req, res, next) {
     res.render('index', { title: 'Mini Messageboard', messages });
 });
